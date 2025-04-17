9229be993e9002acfd3547fe4e0724442af71678
memberRemember <memberremember@MacBook-Air-memberRemember.local>
2025-04-17 06:14:30 +0300

    added imports

--- sdmopriemka/priemka/views/appointments_views.py
+++ sdmopriemka/priemka/views/appointments_views.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse, HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
